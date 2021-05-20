bf4b641cf5e54d9cfa95c21bac2b509f162266d5
jvb93 <976577+jvb93@users.noreply.github.com>
2021-05-19 22:46:03 -0700

    wip message verify with nacl

--- Lambdas/DiscordBot/Function.cs
+++ Lambdas/DiscordBot/Function.cs
@@ -1,16 +1,30 @@
-﻿using Amazon.Lambda.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Chaos.NaCl;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace DiscordBot
 {
     public class Function
     {
-        public APIGatewayProxyResponse Handle(APIGatewayProxyRequest request, ILambdaContext context)
+        public APIGatewayProxyResponse Ping(APIGatewayProxyRequest request, ILambdaContext context)
         {
-             var response = new APIGatewayProxyResponse
+            // todo get this from secrets manager or env
+            var publicKey = "";
+            
+            var signature = request.Headers["X-Signature-Ed25519"];
+            var timestamp = request.Headers["X-Signature-Timestamp"];
+
+            context.Logger.Log($"signature {signature}");
+            context.Logger.Log($"timestamp {timestamp}");
+            
+            var response = new APIGatewayProxyResponse
             {
-                StatusCode = (int)HttpStatusCode.OK,
-                Body = $"Ok",
+                StatusCode = (int)HttpStatusCode.Unauthorized,
+                Body = $"unauthorized",
                 Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
             };
 
