b9e2bf5eb92f660063608b6582f5d2578fdba01c
jvb93 <976577+jvb93@users.noreply.github.com>
2021-05-19 21:29:49 -0700

    added basic hello world lambda

---
+++ Lambdas/DiscordBot/Function.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace DiscordBot
+{
+    public class Function
+    {
+        public APIGatewayProxyResponse Handle(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+             var response = new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = $"Ok",
+                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+            };
+
+            return response;
+        }
+    }
+}
