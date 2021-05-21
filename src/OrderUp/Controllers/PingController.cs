2e6b24a32c86424ef236e4846f22738a420d92c0
jvb93 <1976577+jvb93@users.noreply.github.com>
2021-05-20 21:20:23 -0700

    added sam, ping controller

---
+++ src/OrderUp/Controllers/PingController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrderUp.Controllers
+{
+    [Route("api/[controller]")]
+    public class PingController : ControllerBase
+    {
+        private readonly ILogger<PingController> _logger;
+        
+        public PingController(ILogger<PingController> logger)
+        {
+            _logger = logger;
+        }
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromHeader(Name = "X-Signature-Ed25519")] string signature, [FromHeader(Name = "X-Signature-Timestamp")]string timestamp)
+        {
+            _logger.LogInformation("Signature: {sig}", signature);
+            _logger.LogInformation("Timestamp: {ts}", timestamp);
+
+            return new JsonResult(new {type = 1});
+        }
+    }
+}
